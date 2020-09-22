e2fec2e87c95196d693a4d4b3755f80f6c7c4a84
wsio001 <willwcsio@gmail.com>
2020-09-22 18:23:00 -0400

    Add readMore handling

--- src/Movie.js
+++ src/Movie.js
@@ -9,13 +9,22 @@ const Movie = ({img, title, releaeDate,desc}) => {
         return (`https://image.tmdb.org/t/p/w500${img}`);
     })
 
+    const [readMore, setReadMore] = useState(() => {
+        return false;
+    }); 
+
+    const handleReadMore = () => {
+        setReadMore(!readMore);
+    }
 
     return(
         <div>
             <img className = "poster" src = {imgLink} alt = "" />
             <h3>Title: {title}</h3>
             <p>Release Date: {releaeDate}</p>
-            <p>Description: {desc}</p>
+            {!readMore && <p onClick = {handleReadMore} ><u>Read more</u></p>}
+            {readMore && <p>Description: {desc}</p>}
+            {readMore && <p onClick = {handleReadMore} ><u>Read less</u></p>}
         </div>
     )
 }
