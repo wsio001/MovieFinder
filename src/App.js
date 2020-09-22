8e22ef5942a0e657aa755caab9314ec14da228ab
wsio001 <willwcsio@gmail.com>
2020-09-22 13:59:06 -0400

    finished pagination

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 //import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import Pagination from 'react-bootstrap/Pagination'
+import Pagination from '@material-ui/lab/Pagination'
 import {key} from './key.js';
 import Movie from './Movie.js';
 
@@ -15,6 +15,7 @@ const App = () => {
 
   const API_KEY = key //you can either insert your key here as a string, or make a key.js and type export let key = "INSERTY YOUR KEY";
   const firstRun = useRef(true);
+
   const [movies, setMovies] = useState(()=>{
     return [];
   });
@@ -33,9 +34,9 @@ const App = () => {
   const [totalPg, setTotalPg] = useState(() => {
     return 1;
   });
-  const [command, setCommand] = useState(() => {
-    return '';
-  });
+  const [showPagination, setShowPagination] = useState(() => {
+    return false;
+  }); 
 
   useEffect(() => {
     if(firstRun.current){
@@ -45,43 +46,34 @@ const App = () => {
     getMovies();
   }, [query, activePg]);
 
-  const handlePagination = e => {
+  const handlePagination = (event, value) => {
+    console.log(value)
     console.log("handle page")
-    let command = e.target.id
-    console.log(command)
-    switch(command) {
-      case 'first':
-        setActivePg(1);
-        break;
-      case 'last':
-        setActivePg(totalPg);
-        break;
-      case 'prev':
-        setActivePg(preActivePg => preActivePg - 1);
-        break;
-      case 'next':
-        setActivePg(preActivePg => preActivePg + 1);
-        console.log("active page + 1")
-        break;
-      default:
-        console.log("no command coming in")
-    }
+    setActivePg(value)
   }
   
 
   const getMovies = async() => {
+    if(query === ''){
+      setMsg("Please enter a keyword.")
+      return;
+    }
+
     console.log(query)
     console.log(activePg)
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&page=${activePg}&include_adult=false`);
     const data = await response.json();
     console.log(activePg);
+
     if(data.total_results === 0){
       setMsg("Sorry, we cannot find any results.")
       return;
     }
+
     console.log(data)
     setMovies(data.results);
     setTotalPg(data.total_pages);
+    setShowPagination(true);
     console.log(data.results);
   }
 
@@ -122,16 +114,18 @@ const App = () => {
         desc = {movie.overview}
         />
       ))}
-      <Pagination>
-        <Pagination.First />
-        <Pagination.Prev id = "prev" onClick = {handlePagination} value = "prev" disabled = {activePg === 1}/>
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
-        <Pagination.Next id = 'next' onClick = {handlePagination} value = "next" disabled = {activePg === totalPg}/>
-        <Pagination.Last />
-      </Pagination>
+
+
+     {showPagination && <Pagination count={totalPg} 
+      shape="rounded" 
+      showFirstButton 
+      showLastButton
+      siblingCount={1}
+      boundaryCount={1}
+      defaultPage={1}
+      onChange={handlePagination}
+      page = {activePg}
+      />}
     </div>
   );
 };
